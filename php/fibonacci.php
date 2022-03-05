127bb703a657a59e54c4728a1b5a9cc267e512e7
Ricky Lee <ricky1530@vip.qq.com>
2022-03-05 15:55:02 +0800

    Update fibonacci.php

--- php/fibonacci.php
+++ php/fibonacci.php
@@ -1,17 +1,21 @@
+<!--
+Run time environment : PHP 7.4+
+-->
 <?php
-    $x = array(0,1);
-    for($t=2;$t<=9999;++$t)
+    $x = array(0=>0,1=>1);
+    $N = 1476; //the maximum that can be showed by PHP
+    for($t=2;$t<=$N;++$t)
         {
-           $x[$t]=$x[$t-1]+$[$t-2]
+           $x[]=$x[($t-1)]+$x[($t-2)];
         };    //make the fibonacci array
-
+        
     function show_all_number_in_fibonacci_array() {
-        var_dump($x)
+    global $x;
+        var_dump($GLOBALS['x']);
     };    //show the fibonacci array
-    
+
     function show_one_number_in_fibonacci_array($q) {
-      var_dump($x[$q]);
-    };
-  show_all_number_in_fibonacci_array();
-    
+        global $x;
+        var_dump($x[$q]); 
+    }
 ?>
